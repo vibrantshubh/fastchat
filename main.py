0ff1f57053c177558adc64de358a7124874768a3
vibrantshubh <patilshubh0712@gmail.com>
2025-07-17 17:09:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -147,4 +147,4 @@ if __name__ == "__main__":
         print(f"🌐 Mobile Chat (LAN): {link}")
         show_qr(link)
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
